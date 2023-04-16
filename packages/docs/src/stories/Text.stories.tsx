2f5ea27d2922ed3b3675ecbb0070737f098b6c7a
Willian Del Freu <williankeyex@gmail.com>
2023-04-16 15:38:57 -0300

    ci: Add docs workflow

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,44 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure tempore fugit dicta ducimus et distinctio porro soluta modi aliquam repellat sint necessitatibus cumque accusamus assumenda, consequatur, recusandae a vero inventore.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '3xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
---
+++ packages/react/src/components/Avatar/styles.ts
@@ -0,0 +1,32 @@
+import * as Avatar from '@radix-ui/react-avatar'
+import { styled } from '../../styles'
+
+export const AvatarContainer = styled(Avatar.Root, {
+  borderRadius: '$full',
+  display: 'inline-block',
+  widht: '$12',
+  height: '$12',
+  overflow: 'hidden',
+})
+
+export const AvatarImage = styled(Avatar.Image, {
+  widht: '100%',
+  height: '100%',
+  objectFit: 'cover',
+  borderRadius: 'inherit',
+})
+
+export const AvatarFallback = styled(Avatar.Fallback, {
+  widht: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '$gray600',
+  color: '$gray800',
+
+  svg: {
+    widht: '$6',
+    height: '$6',
+  },
+})
