0bb0845e12ed9214f1fc3449fbf2f1d2222425ac
Willian Del Freu <williankeyex@gmail.com>
2023-04-16 17:57:27 -0300

    Change name of packages

---
+++ .changeset/heavy-terms-promise.md
@@ -0,0 +1,8 @@
+---
+"@williandf-ui/eslint-config": major
+"@williandf-ui/ts-config": major
+"@williandf-ui/tokens": major
+"@williandf-ui/react": major
+---
+
+Add to NPM
--- packages/docs/src/stories/MultiStep.stories.tsx
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryObj, Meta } from '@storybook/react'
-import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+import { Box, MultiStep, MultiStepProps } from '@williandf-ui/react'
 
 export default {
   title: 'Form/Multi Step',
